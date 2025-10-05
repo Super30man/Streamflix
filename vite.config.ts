1aef7e482bd33052ff1a4bb693a5e3747f78d3ad
Super30man <Shanaychy13@gmail.com>
2025-10-05 15:51:50 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/your-repo-name/' : '/',
+  base: mode === 'production' ? 'https://super30man.github.io/Streamflix' : '/',
   server: {
     host: "::",
     port: 8080,
